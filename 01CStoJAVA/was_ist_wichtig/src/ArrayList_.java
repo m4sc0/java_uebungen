122d6901c6a8945f36728d7954a0875bdcf1e728
ploebl <philip.loebl@edvschule-plattling.de>
2023-09-20 11:51:17 +0200

    update ArrayList_

--- 01CStoJAVA/was_ist_wichtig/src/ArrayList_.java
+++ 01CStoJAVA/was_ist_wichtig/src/ArrayList_.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class ArrayList_ {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        double avg = 0;
+        for (int i = 0; i < nums.stream().count(); i++) {
+            avg += nums.get(i);
+        }
+
+        avg = avg / nums.stream().count();
+        System.out.println(avg);
+    }
 }
