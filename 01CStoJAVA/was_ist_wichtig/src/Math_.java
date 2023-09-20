1694de15671246479afae5cc9a9c1ebf4be6b96a
ploebl <philip.loebl@edvschule-plattling.de>
2023-09-20 11:04:33 +0200

    update Math_

--- 01CStoJAVA/was_ist_wichtig/src/Math_.java
+++ 01CStoJAVA/was_ist_wichtig/src/Math_.java
@@ -1,2 +1,12 @@
 public class Math_ {
+    public static void main(String[] args) {
+        int zahl = 15;
+        double quadrat = Math.pow(zahl, zahl);
+        double wurzel = Math.sqrt(zahl);
+        double cube = Math.multiplyExact(Math.multiplyExact(zahl, zahl), zahl);
+
+        System.out.println(quadrat);
+        System.out.println(wurzel);
+        System.out.println(cube);
+    }
 }
