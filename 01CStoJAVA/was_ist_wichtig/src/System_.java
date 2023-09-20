e375b395396e2cc2478c0250c5fe7de42d47b587
ploebl <philip.loebl@edvschule-plattling.de>
2023-09-20 11:12:46 +0200

    update System_

--- 01CStoJAVA/was_ist_wichtig/src/System_.java
+++ 01CStoJAVA/was_ist_wichtig/src/System_.java
@@ -2,7 +2,10 @@ public class System_ {
     public static void main(String[] args) {
         long before = System.currentTimeMillis();
 
-        Math_.Testing();
+        long sum = 0;
+        for (long i = 0; i < 1_000_000_000; i++) {
+            sum += i;
+        }
 
         long now = System.currentTimeMillis();
 
