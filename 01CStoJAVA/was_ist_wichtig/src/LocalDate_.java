d8b914beb4dce0ee65a4ad90f65445ef88e9f31f
ploebl <philip.loebl@edvschule-plattling.de>
2023-09-20 13:53:20 +0200

    update LocalDate_

--- 01CStoJAVA/was_ist_wichtig/src/LocalDate_.java
+++ 01CStoJAVA/was_ist_wichtig/src/LocalDate_.java
@@ -1,2 +1,13 @@
+import java.time.LocalDate;
+
 public class LocalDate_ {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+
+        System.out.println(date);
+
+        date = date.plusDays(100);
+
+        System.out.println(date);
+    }
 }
